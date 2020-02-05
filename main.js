ea9f569b8a435e6543a4185143f399dd9a77506e
Luan Martins <48186167+muanlartins@users.noreply.github.com>
2020-02-05 17:24:43 -0300

    Add main header
    Responsive modification with bootstrap and javascript onresize

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+window.onload = resizeFunction;
 window.onscroll = function() {scrollFunction()};
+window.onresize = resizeFunction;
 
 function scrollFunction(){
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
@@ -8,5 +10,36 @@ function scrollFunction(){
     else{
         document.getElementById("navbar-main").classList.remove("navbar-background");
         document.getElementById("logo").style.fontSize ="32px";
+    }
+}
+
+function resizeFunction(){
+    if (window.innerWidth >= 992){
+        if (!document.getElementById("text-1").classList.contains("display-4")){
+            document.getElementById("text-1").classList.add("display-4");
+            document.getElementById("text-1").classList.remove("h1");
+        }
+        if (!document.getElementById("text-2").classList.contains("h3")){
+            document.getElementById("text-2").classList.add("h3");
+            document.getElementById("text-2").classList.remove("h4");
+        }
+        if (!document.getElementById("text-3").classList.contains("h5")){
+            document.getElementById("text-3").classList.add("h5");
+            document.getElementById("text-3").classList.remove("h6");
+        }
+        if (!document.getElementById("text-4").classList.contains("h4")){
+            document.getElementById("text-4").classList.add("h4");
+            document.getElementById("text-4").classList.remove("h5");
+        }
+    }
+    else{
+        document.getElementById("text-1").classList.remove("display-4");
+        document.getElementById("text-1").classList.add("h1");
+        document.getElementById("text-2").classList.remove("h3");
+        document.getElementById("text-2").classList.add("h4");
+        document.getElementById("text-3").classList.remove("h5");
+        document.getElementById("text-3").classList.add("h6");
+        document.getElementById("text-4").classList.remove("h4");
+        document.getElementById("text-4").classList.add("h5");
     }
 }
