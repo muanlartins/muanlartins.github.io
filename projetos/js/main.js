972bb4bb6820be752e9c968b54e30b9cfa43d856
Luan Martins <48186167+muanlartins@users.noreply.github.com>
2020-02-10 12:51:53 -0300

    Added more effects and fixed navbar

--- projetos/js/main.js
+++ projetos/js/main.js
@@ -11,6 +11,12 @@ function imagePicker(){
             document.getElementById("piece-4").src = "img/white-king.png";
             document.getElementById("piece-5").src = "img/white-king.png";
             document.getElementById("piece-6").src = "img/black-queen.png";
+            document.getElementById("piece-7").src="img/black-king.png";
+            document.getElementById("piece-8").src = "img/white-queen.png";
+            document.getElementById("piece-9").src = "img/black-queen.png";
+            document.getElementById("piece-10").src = "img/white-king.png";
+            document.getElementById("piece-11").src = "img/white-king.png";
+            document.getElementById("piece-12").src = "img/black-queen.png";
             break;
         case 2:
             document.getElementById("piece-1").src = "img/leaf-1.png";
@@ -19,12 +25,24 @@ function imagePicker(){
             document.getElementById("piece-4").src = "img/leaf-2.png";
             document.getElementById("piece-5").src = "img/leaf-1.png";
             document.getElementById("piece-6").src = "img/leaf-3.png";
-            document.getElementById("piece-1").style.width += "60px";
-            document.getElementById("piece-2").style.width += "60px";
-            document.getElementById("piece-3").style.width += "60px";
-            document.getElementById("piece-4").style.width += "60px";
-            document.getElementById("piece-5").style.width += "60px";
-            document.getElementById("piece-6").style.width += "60px";
+            document.getElementById("piece-1").style.width += "80px";
+            document.getElementById("piece-2").style.width += "80px";
+            document.getElementById("piece-3").style.width += "80px";
+            document.getElementById("piece-4").style.width += "80px";
+            document.getElementById("piece-5").style.width += "80px";
+            document.getElementById("piece-6").style.width += "80px";
+            document.getElementById("piece-7").src = "img/leaf-1.png";
+            document.getElementById("piece-8").src = "img/leaf-2.png";
+            document.getElementById("piece-9").src = "img/leaf-3.png";
+            document.getElementById("piece-10").src = "img/leaf-2.png";
+            document.getElementById("piece-11").src = "img/leaf-1.png";
+            document.getElementById("piece-12").src = "img/leaf-3.png";
+            document.getElementById("piece-7").style.width += "80px";
+            document.getElementById("piece-8").style.width += "80px";
+            document.getElementById("piece-9").style.width += "80px";
+            document.getElementById("piece-10").style.width += "80px";
+            document.getElementById("piece-11").style.width += "80px";
+            document.getElementById("piece-12").style.width += "80px";
             break;
     }
 }
